87f040b8fd026b6e4db83168a20879d6abda3abd
Alafandri <131587531+Alafandri@users.noreply.github.com>
2026-06-16 17:02:58 +0300

    shake

--- scissors.gd
+++ scissors.gd
@@ -5,6 +5,9 @@ var of = Vector2(0,0)
 var start_position: Vector2
 var end_position: Vector2
 
+var tween: Tween
+var tween2: Tween
+
 func _ready() -> void:
 	start_position = global_position
 
@@ -16,10 +19,39 @@ func _process(_delta):
 
 func _on_button_button_down() -> void:
 	dragging = true
+
+	tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(1.25, 1.25), 0.05)
+
+	tween2 = create_tween()
+	tween2.set_loops()
+	tween2.tween_property(self, "rotation_degrees", 5.0, 0.25)
+	tween2.tween_property(self, "rotation_degrees", -5.0, 0.4)
+	tween2.tween_property(self, "rotation_degrees", 0.0, 0.25)
+
 	of = get_global_mouse_position() - global_position
 
 func _on_button_button_up() -> void:
 	dragging = false
+
+	if tween2:
+		tween2.kill()
+
+	rotation_degrees = 0
+
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(1, 1), 0)
+
 	end_position = global_position
 	print(end_position)
 	position = start_position
+
+
+func _on_button_mouse_entered() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.1)
+
+
+func _on_button_mouse_exited() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(1, 1), 0.1)
