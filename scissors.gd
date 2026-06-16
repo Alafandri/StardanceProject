24ebba634d7683aee56e50008aa109f615dbe357
Alafandri <131587531+Alafandri@users.noreply.github.com>
2026-06-16 16:49:16 +0300

    fix2

--- scissors.gd
+++ scissors.gd
@@ -1,11 +1,25 @@
 extends Sprite2D
 
+var dragging = false
+var of = Vector2(0,0)
+var start_position: Vector2
+var end_position: Vector2
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	start_position = global_position
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _process(_delta):
+	if dragging:
+		position = get_global_mouse_position() - of
+
+
+func _on_button_button_down() -> void:
+	dragging = true
+	of = get_global_mouse_position() - global_position
+
+func _on_button_button_up() -> void:
+	dragging = false
+	end_position = global_position
+	print(end_position)
+	position = start_position
